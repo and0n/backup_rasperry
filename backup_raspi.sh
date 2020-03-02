b2b4246effd5be9b6501568de953c3258c088893
Donko Dimov <donko@abv.bg>
2020-03-02 12:19:25 +0100

    fix: Change raspi to IP-ADDRESS-RPI

--- backup_raspi.sh
+++ backup_raspi.sh
@@ -43,4 +43,4 @@ DEST_FILE="backup-$(date +%F_%R).img"
 DEST_FOLDER="/backup"
 
 ## Connect with ssh start to copy image of the full disk and save to local folder
-ssh pi@raspi "sudo dd if=/dev/mmcblk0 bs=4M " | dd of=$DEST_FOLDER/$DEST_FILE
+ssh pi@IP-ADDRESS-RPI "sudo dd if=/dev/mmcblk0 bs=4M " | dd of=$DEST_FOLDER/$DEST_FILE
